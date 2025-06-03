8dee8fa57aa2c50dc8439ed27136d1fc320d0d87
Mac <paulbolu16@gmail.com>
2025-06-03 17:18:10 +0100

    testing image upload of sell form

--- public/trustbarter.js
+++ public/trustbarter.js
@@ -13,3 +13,20 @@ function submitForm(e) {
 const getElementVal = (id) => {
     return document.getElementById(id).value;
 }
+
+//sell script
+function preview(input, imgId, placeholderId) {
+    const file = input.files[0];
+    const img = document.getElementById(imgId);
+    const placeholder = document.getElementById(placeholderId);
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        img.src = reader.result;
+        img.classList.remove('hidden');
+        placeholder.classList.add('hidden');
+      };
+      reader.readAsDataURL(file);
+    }
+  }
